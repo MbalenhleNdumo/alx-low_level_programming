ac92e3020687d2852ce349ae15cefb1654ed68b9
Mbalenhle Ndumo <mbalehle257@gmail.com>
2023-07-24 13:32:05 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -13,14 +13,14 @@ void puts2(char *str)
 	int i;
 	int j = 0;
 
-	while (str[j] != '\0')
+	while (str[j] != '\0');
 	{
 	j++;
 	}
 
-	for (i = 0; i < j; i += 2)
+	for (i = 0; i < j; i += 2);
 	{
 		_putchar(str[i]);
 	}
-	_putchar('\n')
+	_putchar('\n');
 }
